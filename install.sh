275f1635b1e2bd38257e6914af3e00d7f5aa0b82
Christoph Niemann <christoph.niemann@tuhh.de>
2014-10-24 17:38:23 +0200

    Added the TUHH stuff for 5.2

--- install.sh
+++ install.sh
@@ -1,5 +1,4 @@
 files="
-conf/conf.xml
 conf/control_panel.xml
 conf/gcs/horizontal.xml
 conf/simulator/jsbsim/aircraft/reset_ristedt.xml
@@ -7,7 +6,10 @@ conf/radios/mc15_CHNI.xml
 conf/joystick/saitek_p1500.xml
 conf/flight_plans/rotorcraft_stieglitz.xml
 conf/flight_plans/versatile_stieglitz.xml
+conf/airframes/Up2U_tuhh.xml
+conf/airframes/katana_tuhh.xml
 conf/airframes/x8_chni.xml
+conf/radios/TGY9x_jeti.xml
 ENV
 sw/airborne/boards/twog_1.0.h
 sw/airborne/boards/tiny_2.1.h
