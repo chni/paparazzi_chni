616d866018c1ffd3760a4975066a96496a31f540
Christoph Niemann <christophniemann@gmx.net>
2014-11-21 02:10:41 +0100

    add joystcik file for dx6i, mode not working. Sim Working with dx6i and HBMini. Switched AUTO2 Mode back to HZH, since NAV also needs selection of block instead of Holding Point. So this is more safe.

--- install.sh
+++ install.sh
@@ -3,6 +3,7 @@ conf/control_panel.xml
 conf/simulator/jsbsim/aircraft/reset_ristedt.xml
 conf/radios/mc15_CHNI.xml
 conf/joystick/saitek_p1500.xml
+conf/joystick/dx6i_CHNI.xml
 conf/settings/control/ctl_airspeed.xml
 conf/flight_plans/rotorcraft_stieglitz.xml
 conf/flight_plans/rotorcraft_scharbeutz.xml
